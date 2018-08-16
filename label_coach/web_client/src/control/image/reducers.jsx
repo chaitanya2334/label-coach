f7877dd39a8f1f7ac6f93511ae6f77c0369bdb3e
Chaitanya <chaitanya.kulkarni2334@gmail.com>
2018-08-15 20:57:21 -0400

    fixed draw buttons not working

--- label_coach/web_client/src/control/image/reducers.jsx
+++ label_coach/web_client/src/control/image/reducers.jsx
@@ -2,7 +2,5 @@ import labelReducer from "../label/reducers";
 import produce from "immer";
 
 export function images(images=[], action) {
-    return produce(images, (draftState) => {
-        draftState[action.image_id] = labelReducer(draftState[action.image_id], action)
-    });
+    return images
 }
