73dbdfe671b9f3bb9dc0dc65e35fee5e91374759
chaitanya2334 <chaitanya.kulkarni2334@gmail.com>
2018-12-08 02:45:07 -0500

    restructured brush stroke classes.

--- label_coach/web_client/src/Imager/canvas_overlay/brush.jsx
+++ label_coach/web_client/src/Imager/canvas_overlay/brush.jsx
@@ -1,103 +1,9 @@
-export default class Brush {
+import Stroke from "./stroke";
+
+export default class Brush extends Stroke{
     constructor(overlay, viewer, id, label = null, radius = null, labelFolderId = null, updateStrokes = null) {
-        this.path = [];
-        this.overlay = overlay;
-        this.viewer = viewer;
-        this.canvas = overlay.fabricCanvas();
+        super(overlay, viewer, id, label.color, radius, labelFolderId, updateStrokes);
         this.label = label;
-        this.radius = radius;
-        this.updateStrokes = updateStrokes;
-        this.id = id;
-        this.labelFolderId = labelFolderId;
-        var cursorOpacity = .5;
-        this.mousecursor = new fabric.Circle({
-                                                 left: -100,
-                                                 top: -100,
-                                                 radius: radius / 2,
-                                                 fill: label.color,
-                                                 stroke: "black",
-                                                 originX: 'center',
-                                                 originY: 'center'
-                                             });
-        this.canvas.add(this.mousecursor);
-
-    }
-
-    activate() {
-        this.canvas.freeDrawingBrush.color = this.label.color;
-        this.canvas.freeDrawingBrush.width = this.radius;
-        this.canvas.globalCompositeOperation = "source-over";
-        this.viewer.setMouseNavEnabled(false);
-        this.viewer.outerTracker.setTracking(false);
-        this.canvas.isDrawingMode = true;
-        this.canvas.on('path:created', (e) => {
-            e.path.globalCompositeOperation = 'source-over';
-            // This will not add an SVG CSS class, but at least will allow us to identify
-            // erasures in object list
-            this.canvas.renderAll();
-        });
-        this.canvas.on("mouse:down", () => {
-            if (!this.drawing) {
-                this.canvas.remove(this.mousecursor);
-                this.mousecursor = null;
-
-                this.canvas.renderAll();
-                this.drawing = 1;
-            }
-        });
-        this.canvas.on("mouse:move", (evt) => {
-            if (!this.drawing) {
-                let mouse = this.canvas.getPointer(evt.e);
-                this.mousecursor
-                    .set({
-                             top: mouse.y,
-                             left: mouse.x
-                         })
-                    .setCoords();
-                this.canvas.renderAll();
-            }
-        });
-        this.canvas.on("mouse:out", () => {
-            if (!this.drawing) {
-                this.mousecursor.set({
-                                         top: -100,
-                                         left: -100
-                                     })
-                    .setCoords();
-                this.canvas.renderAll();
-            }
-        });
-        this.canvas.on("mouse:up", () => {
-            if (this.drawing) {
-                this.mousecursor = new fabric.Circle({
-                                                         left: -100,
-                                                         top: -100,
-                                                         radius: this.radius / 2,
-                                                         fill: this.label.color,
-                                                         stroke: "black",
-                                                         originX: 'center',
-                                                         originY: 'center'
-                                                     });
-                this.canvas.add(this.mousecursor);
-                this.canvas.renderAll();
-                this.drawing = 0;
-            }
-            this.saveToImage();
-            this.canvas.__eventListeners["mouse:up"] = [];
-        });
-
-    }
-
-    deactivate() {
-        this.viewer.setMouseNavEnabled(true);
-        this.viewer.outerTracker.setTracking(true);
-        this.canvas.isDrawingMode = false;
-        this.canvas.__eventListeners["mouse:up"] = [];
-        this.canvas.__eventListeners["mouse:created"] = [];
-        this.canvas.__eventListeners["object:added"] = [];
-        this.canvas.__eventListeners["mouse:out"] = [];
-        this.canvas.__eventListeners["mouse:move"] = [];
-        this.canvas.__eventListeners["mouse:move"] = [];
     }
 
     saveToImage() {
@@ -112,16 +18,5 @@ export default class Brush {
         this.id++;
 
 
-    }
-
-    loadFromImage(imgSrc, transform) {
-        fabric.loadSVGFromURL(imgSrc, (objects, options) => {
-            let obj = fabric.util.groupSVGElements(objects, options);
-            this.canvas.add(obj);
-            obj.setCoords();
-
-            this.canvas.renderAll();
-            this.canvas.viewportTransform = transform;
-        });
     }
 }
