32223229e86e932cc9322002cad08014cccfee91
chaitanya2334 <chaitanya.kulkarni2334@gmail.com>
2018-12-08 01:37:24 -0500

    fixed eraser

--- label_coach/web_client/src/Imager/canvas_overlay/stroke.jsx
+++ label_coach/web_client/src/Imager/canvas_overlay/stroke.jsx
@@ -1,85 +1,30 @@
 export default class Stroke {
-    constructor(overlay, viewer, id, repColor, cursorColor, radius = null, labelFolderId = null, updateStrokes = null,
-                updateLabelImage = null, genMask = false) {
+    constructor(overlay, viewer, id, cursorColor, radius = null, labelFolderId = null, updateStrokes = null) {
         this.path = [];
-        this.genMask = genMask;
         this.overlay = overlay;
         this.viewer = viewer;
         this.canvas = overlay.fabricCanvas();
         this.cursorColor = cursorColor;
-        this.repColor = repColor;
-        this.zoom = this.viewer.viewport.getZoom(true);
-        this.radius = radius / this.zoom;
+        this.radius = radius;
         this.updateStrokes = updateStrokes;
         this.id = id;
-
         this.labelFolderId = labelFolderId;
-        this.updateLabelImage = updateLabelImage;
         this.mousecursor = new fabric.Circle({
                                                  left: -100,
                                                  top: -100,
-                                                 radius: this.radius,
+                                                 radius: radius / 2,
                                                  fill: this.cursorColor,
                                                  stroke: "black",
-                                                 strokeWidth: 2 / this.zoom,
                                                  originX: 'center',
                                                  originY: 'center'
                                              });
         this.canvas.add(this.mousecursor);
-        if (this.genMask) {
-            this.maskImage = this.maskify();
-        }
 
     }
-
-    static hexToRgb(hex) {
-        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    }
-
-    static toBase64(img, width, height) {
-        let canvas = document.createElement("canvas");
-        canvas.width = width;
-        canvas.height = height;
-        canvas.getContext("2d")
-              .putImageData(img, 0, 0);
-        return canvas.toDataURL();
-    }
-
-    maskify() {
-        let canvas = this.overlay.getFullCanvas();
-        let ctx = canvas.getContext();
-        let srcImg = canvas.getContext()
-                           .getImageData(0, 0, canvas.width, canvas.height);
-        let dstImg = ctx.createImageData(srcImg);
-        let srcData = srcImg.data;
-        let strokeColor = Stroke.hexToRgb(this.repColor);
-        console.log(strokeColor);
-        for (let i = 0, maxI = srcData.length; i < maxI; i += 4) {
-            if (srcData[i] === strokeColor.r &&
-                srcData[i + 1] === strokeColor.g &&
-                srcData[i + 2] === strokeColor.b &&
-                srcData[i + 3] === 255) {
-                //white
-                dstImg.data[i] = dstImg.data[i + 1] = dstImg.data[i + 2] = 255;
-            } else {
-                //black
-                dstImg.data[i] = dstImg.data[i + 1] = dstImg.data[i + 2] = 0;
-            }
-            //no transparency
-            dstImg.data[i + 3] = 255;
-        }
-        return Stroke.toBase64(dstImg, canvas.width, canvas.height);
-    }
-
 
     activate() {
         this.canvas.freeDrawingBrush.color = this.cursorColor;
-        this.canvas.freeDrawingBrush.width = this.radius*2;
+        this.canvas.freeDrawingBrush.width = this.radius;
         this.canvas.globalCompositeOperation = "source-over";
         this.viewer.setMouseNavEnabled(false);
         this.viewer.outerTracker.setTracking(false);
@@ -126,10 +71,9 @@ export default class Stroke {
                 this.mousecursor = new fabric.Circle({
                                                          left: -100,
                                                          top: -100,
-                                                         radius: this.radius,
+                                                         radius: this.radius / 2,
                                                          fill: this.cursorColor,
                                                          stroke: "black",
-                                                         strokeWidth: 2 / this.zoom,
                                                          originX: 'center',
                                                          originY: 'center'
                                                      });
@@ -138,7 +82,6 @@ export default class Stroke {
                 this.drawing = 0;
             }
             this.saveToImage();
-
             this.canvas.__eventListeners["mouse:up"] = [];
         })
     }
@@ -149,10 +92,6 @@ export default class Stroke {
         this.canvas.isDrawingMode = false;
         this.canvas.__eventListeners["mouse:up"] = [];
         this.canvas.__eventListeners["mouse:created"] = [];
-        this.canvas.__eventListeners["object:added"] = [];
-        this.canvas.__eventListeners["mouse:out"] = [];
-        this.canvas.__eventListeners["mouse:move"] = [];
-        this.canvas.__eventListeners["mouse:move"] = [];
     }
 
     saveToImage() {
