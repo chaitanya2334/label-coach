ff5d12901b98493f4748f90072a073c3ae0dd41e
chaitanya2334 <chaitanya.kulkarni2334@gmail.com>
2019-02-28 15:07:42 -0500

    fixed issue with crashing of the tool on selecting the eraser.

--- label_coach/web_client/src/Imager/canvas_overlay/stroke.jsx
+++ label_coach/web_client/src/Imager/canvas_overlay/stroke.jsx
@@ -9,7 +9,7 @@ export default class Stroke {
         this.radius = radius;
         this.updateStrokes = updateStrokes;
         this.id = id;
-        this.maskImage = this.maskify();
+
         this.labelFolderId = labelFolderId;
         this.updateLabelImage = updateLabelImage;
         this.mousecursor = new fabric.Circle({
@@ -42,31 +42,7 @@ export default class Stroke {
         return canvas.toDataURL();
     }
 
-    maskify() {
-        let canvas = this.overlay.getFullCanvas();
-        let ctx = canvas.getContext();
-        let srcImg = canvas.getContext()
-                         .getImageData(0, 0, canvas.width, canvas.height);
-        let dstImg = ctx.createImageData(srcImg);
-        let srcData = srcImg.data;
-        let strokeColor = Stroke.hexToRgb(this.cursorColor);
-        console.log(strokeColor);
-        for (let i = 0, maxI = srcData.length; i < maxI; i += 4) {
-            if (srcData[i] === strokeColor.r &&
-                srcData[i + 1] === strokeColor.g &&
-                srcData[i + 2] === strokeColor.b &&
-                srcData[i + 3] === 255) {
-                //white
-                dstImg.data[i] = dstImg.data[i + 1] = dstImg.data[i + 2] = 255;
-            } else {
-                //black
-                dstImg.data[i] = dstImg.data[i + 1] = dstImg.data[i + 2] = 0;
-            }
-            //no transparency
-            dstImg.data[i + 3] = 255;
-        }
-        return Stroke.toBase64(dstImg, canvas.width, canvas.height);
-    }
+
 
     activate() {
         this.canvas.freeDrawingBrush.color = this.cursorColor;
