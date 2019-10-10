853df61183e1d018dee84a85c9d06675eec23c4c
chaitanya2334 <chaitanya.kulkarni2334@gmail.com>
2019-10-10 17:25:03 -0400

    fixed long stalls for every tile request made by OSD.

--- label_coach/server/deepzoom.py
+++ label_coach/server/deepzoom.py
@@ -12,19 +12,24 @@ def slugify(text):
     return re.sub('[^a-z0-9]+', '-', text)
 
 
-def load_slide(slidefile):
-
+def load_slide(slidefile, tile_size):
     print("there is an image")
     slide = open_slide(slidefile)
     slides = {
-        "slide": DeepZoomGenerator(slide, tile_size=254, overlap=1, limit_bounds=True)
+        "slide": DeepZoomGenerator(slide,
+                                   tile_size=tile_size,
+                                   overlap=1,
+                                   limit_bounds=True)
     }
     associated_images = []
     slide_properties = slide.properties
     for name, image in slide.associated_images.items():
         associated_images.append(name)
         slug = slugify(name)
-        slides[slug] = DeepZoomGenerator(ImageSlide(image), tile_size=254, overlap=1, limit_bounds=True)
+        slides[slug] = DeepZoomGenerator(ImageSlide(image),
+                                         tile_size=tile_size,
+                                         overlap=1,
+                                         limit_bounds=True)
     try:
         mpp_x = slide.properties[openslide.PROPERTY_NAME_MPP_X]
         mpp_y = slide.properties[openslide.PROPERTY_NAME_MPP_Y]
@@ -36,4 +41,5 @@ def load_slide(slidefile):
 
 
 if __name__ == '__main__':
-    print(load_slide("../web_client/static/images/TCGA-CH-5765-11A-01-TS1.2a1faf76-526b-4581-b947-e8d733674df7.svs"))
+    print(load_slide(
+        "../web_client/static/images/TCGA-CH-5765-11A-01-TS1.2a1faf76-526b-4581-b947-e8d733674df7.svs"))
