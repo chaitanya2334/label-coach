e992bf5ae82239641e8deb4fcd444ce6b5bcf30f
chaitanya2334 <chaitanya.kulkarni2334@gmail.com>
2019-10-22 22:08:10 -0400

    fixed line and polygon tool

--- label_coach/web_client/src/Imager/svg_overlay/line.jsx
+++ label_coach/web_client/src/Imager/svg_overlay/line.jsx
@@ -5,20 +5,48 @@ import * as d3 from 'd3';
 import Dot from "./dot";
 
 export default class Line extends Shape {
-    constructor(overlay, viewer, labelId, lineId, zoom) {
+    constructor(overlay, viewer, label, lineId, zoom, addLine) {
         super(overlay, viewer);
         this.lineId = lineId;
-        this.labelId = labelId;
+        this.labelId = label.id;
         this.zoom = zoom;
         this.strokeWidth = 0.002;
-        this.d3obj = d3.select(this.overlay.node())
+        this.d3obj = d3.select(this.overlay.getNode(0))
                        .append("path")
+                       .attr('stroke', label.color)
+                       .attr('id', this.labelId + "_" + this.lineId)
                        .attr('class', 'transparent')
                        .attr('id', this.lineId)
                        .attr('stroke-width', this.strokeWidth * (1 / this.zoom));
         this.dots = [];
         this.setDrawState("read-only");
+        this.addLine = addLine;
+        this.curveTension = 1;
+        this.curveType = d3.curveCardinalClosed.tension(this.curveTension);
+    }
 
+    onEnter() {
+
+    }
+
+    onExit() {
+        document.body.style.cursor = "auto";
+    }
+
+    onMouseMove(vpPoint) {
+        document.body.style.cursor = "crosshair";
+
+    }
+
+    onDrag(vpPoint){
+        this.appendDot(vpPoint);
+        this.curveType = d3.curveCardinalOpen.tension(this.curveTension);
+    }
+
+    onDragEnd(){
+        // connect back to the first point
+        this.curveType = d3.curveCardinalClosed.tension(this.curveTension);
+        this.save();
     }
 
     setDrawState(state) {
@@ -34,30 +62,32 @@ export default class Line extends Shape {
     }
 
     addImagePoints(points) {
-        for (let point of points) {
-            let imgPoint = new OpenSeadragon.Point(parseInt(point.x), parseInt(point.y));
-            let vpPoint = this.viewer.viewport.imageToViewportCoordinates(imgPoint);
-            this.appendDot(vpPoint);
+        if (points !== undefined) {
+            for (let point of points) {
+                let imgPoint = new OpenSeadragon.Point(parseInt(point.x), parseInt(point.y));
+                let vpPoint = this.viewer.viewport.imageToViewportCoordinates(imgPoint);
+                this.appendDot(vpPoint);
+            }
         }
     }
 
     appendDot(vpPoint) {
         this.dots.push(new Dot(this.overlay, this.viewer, this, this.dots.length, vpPoint, this.zoom, false));
-        this.draw(d3.curveCardinalOpen);
+        this.draw();
     }
 
-    draw(curveType) {
+    draw() {
         let points = [];
         for (let dot of this.dots) {
             points.push([dot.p.x, dot.p.y]);
         }
-        if(this.labelId === 0 && this.lineId === 2) {
-            console.log(curveType);
+        if (this.labelId === 0 && this.lineId === 2) {
+            console.log(this.curveType);
             console.log(this.drawState);
         }
         this.d3obj.datum(points)
             .attr('d', d3.line()
-                           .curve(curveType));
+                         .curve(this.curveType));
     }
 
     save() {
@@ -66,7 +96,8 @@ export default class Line extends Shape {
         this.complete = true;
         this.selected = false;
         this.selectedDot = false;
-        this.draw(d3.curveCardinalClosed);
+        this.draw();
+        this.addLine(this.labelId, this.lineId, this.getImagePoints());
     }
 
     delete() {
